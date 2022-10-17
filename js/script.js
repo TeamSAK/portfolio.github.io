eb9b5baaa6b3d19ba6f635977c4dd5a4350f3d00
SAIF ALI KHAN <92135574+TeamSAK@users.noreply.github.com>
2022-10-17 15:29:11 -0700

    my protfolio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* *********************** typing animation ********************/ 
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Graphic Desginer","Youtuber"],
+    typedSpeed:100,
+    BackSpeed:60,
+    loop:true, 
+})
