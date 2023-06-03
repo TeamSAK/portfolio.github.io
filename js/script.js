6fa39ab4198fed9b4f34e2a7bb87e1f04773bbdd
SAIF ALI KHAN <92135574+TeamSAK@users.noreply.github.com>
2023-06-03 21:20:53 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /* *********************** typing animation ********************/ 
 var typed = new Typed(".typing", {
-strings: ["", "Python Developer", "Web Developer", "Graphic Designer", "Youtuber"],
-typeSpeed: 100,
+strings: [ "Python Developer", "Web Developer", "Graphic Designer", "Youtuber"],
+typeSpeed: 150,
 backSpeed: 60,
 loop: true,
 });
